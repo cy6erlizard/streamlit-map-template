2e719c2ca8e6ad923d18a392c560ab8e168ca0b7
cy6erlizard <yousrihmd@gmail.com>
2024-06-22 03:27:47 +0100

    init

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -1,30 +1,37 @@
 import streamlit as st
+import pandas as pd
 import leafmap.foliumap as leafmap
+
+# State management for map center coordinates
+if "map_center" not in st.session_state:
+    st.session_state["map_center"] = [50, 8]  # Initial center coordinates
 
 st.set_page_config(layout="wide")
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
+st.title("Interactive CO2 Emission Heatmap")
 
-st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+# Load CSV data into a Pandas DataFrame
+df = pd.read_csv("https://raw.githubusercontent.com/cy6erlizard/c_chain_files/main/heatmap.csv")
+df['start_time'] = pd.to_datetime(df['start_time'])
 
-st.title("Heatmap")
+# Extract unique years from the DataFrame
+unique_years = df['start_time'].dt.year.unique()
 
-with st.expander("See source code"):
-    with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4)
-        m.add_heatmap(
-            filepath,
-            latitude="latitude",
-            longitude="longitude",
-            value="pop_max",
-            name="Heat map",
-            radius=20,
-        )
+# Create a slider to select the year
+selected_year = st.slider("Select Year:", min_value=unique_years.min(), max_value=unique_years.max(), value=unique_years[0])
+
+# Filter DataFrame based on the selected year
+df_filtered = df[df['start_time'].dt.year == selected_year]
+
+# Define the heatmap configuration
+m = leafmap.Map(center=st.session_state["map_center"], zoom=5.5)
+m.add_heatmap(
+    data=df_filtered,
+    latitude="lat",
+    longitude="lon",
+    value="pop_max",  # Assuming "pop_max" represents CO2 emission values
+    name="CO2 Emission Heatmap",
+    radius=40,
+)
+m.add_basemap("SATELLITE")
 m.to_streamlit(height=700)
